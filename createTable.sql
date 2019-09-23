08f590cb75b1770c4bf125f38f707fa64fa0253a
Ravi Kumar <46610734+479BetaCoder@users.noreply.github.com>
2019-09-22 21:05:39 -0400

    update create table
    update1

--- createTable.sql
+++ createTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `User` (
-  `id` binary(16) NOT NULL,
+  `id` varchar(36) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
-  `password` varchar(60) NOT NULL COMMENT 'User Table updated',
+  `password` varchar(100) NOT NULL COMMENT 'User Table updated',
   `email_address` varchar(320) NOT NULL,
   `account_created` datetime DEFAULT NULL,
   `account_updated` datetime DEFAULT NULL COMMENT 'User Table',
